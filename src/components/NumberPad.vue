5bfee3219862f5484b2241972b808110daf64042
Dylan <1422554376@qq.com>
2022-11-25 19:42:54 +0800

    使用sync

--- src/components/NumberPad.vue
+++ src/components/NumberPad.vue
@@ -1,29 +1,58 @@
 <template>
     <div class="numberPad"> 
-      <div class="output">100</div>
+      <div class="output">{{output}}</div>
       <div class="buttons">
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>删除</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>清空</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
+      <button @click="inputContent">1</button>
+      <button @click="inputContent">2</button>
+      <button @click="inputContent">3</button>
+      <button @click="remove">删除</button>
+      <button @click="inputContent">4</button>
+      <button @click="inputContent">5</button>
+      <button @click="inputContent">6</button>
+      <button @click="clear">清空</button>
+      <button @click="inputContent">7</button>
+      <button @click="inputContent">8</button>
+      <button @click="inputContent">9</button>
       <button class="ok">ok</button>
-      <button class="zero">0</button>
-      <button>.</button>
+      <button class="zero" @click="inputContent">0</button>
+      <button @click="inputContent">.</button>
     </div>
     </div>
 </template>
 
 <script lang="ts">
-    export default {
-        name:"NumberPad"
+import Vue from 'vue'
+import {Component,Prop} from 'vue-property-decorator'
+@Component
+export default class NumberPad extends Vue{
+  @Prop() readonly value!:number
+  output:string = this.value.toString()
+  inputContent(event:MouseEvent){
+    const button = (event.target as HTMLButtonElement)
+    const input = button.textContent as string
+    if(this.output.length === 16){return}
+    if(this.output === '0'){
+      if('0123456789'.indexOf(input) >= 0){
+        this.output = input
+      }
     }
+    else if(input === '.' && this.output.indexOf('.') >= 0){return}
+    else {this.output += input}
+  }
+  remove(){
+    if(this.output.length === 1){
+      this.output = '0'
+    }
+    console.log(1)
+    this.output = this.output.slice(0,-1)
+    console.log(this.output)
+  }
+  clear(){this.output = '0'}
+  ok(){
+    this.$emit('update:value',this.output)
+    this.$emit('submit',this.output)
+  }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -33,6 +62,7 @@
     font-size: 36px;
     font-family: Consolas,monospace; //等宽字体
     padding: 9px 16px;
+    height:72px;
     text-align: right;
     @extend %innerShadow;
   }
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,24 +1,41 @@
 <template>
   <Layout class-prefix="layout">
-    <number-pad/>
-    <types :xxx="111"/>
-    <notes/>
-    <Tags/>
+    <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
+    <types :type.sync="record.type"/>
+    <Notes @update:value="onUpdateNotes"/>
+    <Tags :data-source.sync="tags" @update:selected="onUpdateTags"/>
   </Layout>
 </template>
 
 <script lang='ts'>
+import Vue from 'vue';
 import NumberPad from '../components/NumberPad.vue';
 import Types from '../components/Types.vue';
 import Notes from '../components/Notes.vue';
 import Tags from '../components/Tags.vue';
-  export default {
-    name:'Money',
-    components:{NumberPad,Types,Notes,Tags}
+import { Component} from 'vue-property-decorator'
+type Record={
+  tags:string[],
+  notes:string,
+  type:string,
+  amount:number
+}
+@Component(
+  {components:{NumberPad,Types,Notes,Tags}})
+  export class  Money extends Vue{
+    tags = ['衣','食','住','行']
+    recordList:Record[] = [] 
+    record:Record = {tags:[],notes:'',type:'-',amount:0}
+    onUpdateTags(tags:string[]){this.record.tags = tags}
+    onUpdateNotes(notes:string){this.record.notes = notes}
+    onUpdateType(type:string){this.record.type = type}
+    onUpdateAmount(value:string){this.record.amount = parseFloat(value)}
+    saveRecord(amount:Number){this.recordList.push(this.record)}
   }
 </script>
+
 <style lang="scss">
-  .layout-content{
+  .layout-content{ 
     display: flex;
     flex-direction: column-reverse;
   }
